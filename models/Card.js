687defcaa71ec2b9f08e7adbaba44b12b751ab27
Nhat <sirnhat0@gmail.com>
2025-08-09 23:44:38 +0700

    first commit

---
+++ models/Card.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const cardSchema = new mongoose.Schema({
+	deck_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Deck', required: true}, 
+	name: {type: String, required: true},
+	definition: {type: String, required: true},
+	hint: {type: String, default: ''},
+	category: {type: [String], default: []},
+	frequency: {type: Number, min: 1, max: 5, default: 3},
+}, {timestamps: true})
+
+module.exports = mongoose.model('Card', cardSchema)
