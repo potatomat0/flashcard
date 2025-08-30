8f93eb216c79628b08ba8c230d8c101784f88117
Nhat <sirnhat0@gmail.com>
2025-08-30 11:17:40 +0700

    fix get profile endpoint

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const { validateRegistration } = require('../middleware/validationMiddleware');
 router.post('/register', validateRegistration, asyncHandler(userController.register));
 router.post('/login', asyncHandler(userController.login));
 
-router.get('/profile', auth, asyncHandler(userController.getUserProfile));
+router.get('/profile', auth, userController.getUserProfile);
 router.patch('/profile', auth, asyncHandler(userController.updateUserProfile));
 router.delete('/profile', auth, asyncHandler(userController.deleteUser));
 
